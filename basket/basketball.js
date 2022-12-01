a4dfeafa47a34db3e3fcf9312d0f4d9afab2fc75
cloydagreat <114746956+cloydagreat@users.noreply.github.com>
2022-12-01 03:21:51 +0100

    i redesigned the score-sheet

---
+++ basket/basketball.js
@@ -0,0 +1,36 @@
+//basket ball javaScript
+let count = 0;
+let somev = document.getElementById("demo");
+let acount = 0;
+let anodav =document.getElementById("demi");
+
+function plus1(){
+	count += 1;
+	somev.innerHTML= count;
+}
+
+function plus2(){
+	count += 2;
+	somev.innerHTML= count;
+}
+
+function plus3(){
+	count += 3;
+	somev.innerHTML= count;
+}
+
+
+function aplus1(){
+	acount += 1;
+	anodav.innerHTML= acount;
+}
+
+function aplus2(){
+	acount += 2;
+	anodav.innerHTML= acount;
+}
+
+function aplus3(){
+	acount += 3;
+	anodav.innerHTML= acount;
+}
